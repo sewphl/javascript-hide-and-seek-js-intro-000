4e4c573599cfb883b1a2e836ad664d2ef79dcc8d
sharon <sharonoptsout@gmail.com>
2018-04-26 23:01:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,3 +6,10 @@ function nestedTarget() {
   var g = document.querySelector("#nested");
   return(g.querySelector(".target"));
 }
+
+function increaseRankBy(n) {
+  const lis = document.querySelectorAll('.ranked-list')
+  for (let i = 0; i < lis.length; i++) {
+    lis[i].innerHTML = (i + 1).toString()
+  }
+}
