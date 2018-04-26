ffcf785bb35eacf4546a953c0a4e23aaaa1ace20
sharon <sharonoptsout@gmail.com>
2018-04-26 22:55:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 function getFirstSelector(selector) {
   return(document.querySelector(selector));
 }
+
+function nestedTarget() {
+  var g = document.querySelector("#nested");
+  return(g.querySelector(".target"));
+}
