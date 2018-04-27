847677aeeb84a133f901916ab3dc7d11b7ab2c04
sharon <sharonoptsout@gmail.com>
2018-04-27 00:01:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,8 +17,9 @@ function increaseRankBy(n) {
 function deepestChild() {
   const ancestor = document.querySelectorAll("#grand-node");
   const desiredNode = ancestor.children[0];
-  for (let i = 0; i < ancestor.length; i++) {
-    var e = ancestor[i].querySelector("div");
-    return(e.innerHTML());
+  for (var i = 0; !desiredNode.children[i]; i) {
+    desiredNode = desiredNode.children[0]
+    }
+    return desiredNode;
   }
 }
